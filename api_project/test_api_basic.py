631269987cb1aedce9ae56f468a3d50abbecaa38
schevcha <j.shevcha@gmail.com>
2025-07-26 01:06:40 +0300

    Initial commit

---
+++ api_project/test_api_basic.py
@@ -0,0 +1,57 @@
+import requests
+
+def test_get_users(base_url):
+    response = requests.get(f"{base_url}/users?page=2")
+    assert response.status_code == 200
+    data = response.json()
+    assert 'data' in data
+    assert isinstance(data["data"], list)
+
+def test_create_user(base_url, headers):
+
+    payload = {
+        "name": "Юля",
+        "job": "QA Engineer"
+    }
+
+    response = requests.post(f"{base_url}/users", json=payload, headers=headers)
+
+    assert response.status_code == 201
+    data = response.json()
+    assert data["name"] == "Юля"
+    assert data["job"] == "QA Engineer"
+    assert "id" in data
+    assert "createdAt" in data
+
+def test_update_user(base_url, headers):
+    payload = {
+        "name": "Юля",
+        "job": "Lead QA"
+    }
+
+    response = requests.put(f"{base_url}/users/2", json=payload, headers=headers)
+
+    assert response.status_code == 200
+    data = response.json()
+    assert data["name"] == "Юля"
+    assert data["job"] == "Lead QA"
+    assert "updatedAt" in data
+
+def test_delete_user(base_url, headers):
+
+    response = requests.delete(f"{base_url}/users/2", headers=headers)
+
+    assert response.status_code == 204  # Нет содержимого, но успешно
+
+def test_create_user_without_body(base_url, headers):
+    response = requests.post(f"{base_url}/users", headers=headers)
+    assert response.status_code == 400 or response.status_code == 415  # зависит от API
+
+def test_get_nonexistent_resource(base_url, headers):
+    response = requests.get(f"{base_url}/unknown/999", headers=headers)
+    assert response.status_code == 404
+
+def test_with_invalid_api_key(base_url):
+    headers = {"x-api-key": "invalid-key"}
+    response = requests.get(f"{base_url}/users/2", headers=headers)
+    assert response.status_code == 401
---
+++ login_project/tests/test_login.py
@@ -0,0 +1,23 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from pages.login_page import LoginPage
+
+def test_successful_login(page):
+    login_page = LoginPage(page)
+    login_page.goto()
+    login_page.login('student', 'Password123')
+
+     # Проверка: заголовок и URL
+    page.wait_for_url("**/logged-in-successfully/")
+    assert page.locator("h1").inner_text() == "Logged In Successfully"
+
+def test_invalid_login(page):
+    login_page = LoginPage(page)
+    login_page.goto()
+    login_page.login("invalid_user", "wrongpass")
+
+    # Проверка сообщения об ошибке
+    error_message = page.locator("div#error").inner_text()
+    assert "Your username is invalid!" in error_message
