b6d9ec8b29a42831fd2f974fe9e72605405b688e
schevcha <j.shevcha@gmail.com>
2025-07-28 04:46:33 +0300

    join ui & api runs

--- api_project/test_api_basic.py
+++ api_project/test_api_basic.py
@@ -38,21 +38,21 @@ def test_update_user(base_url, headers):
     assert data["job"] == "Lead QA"
     assert "updatedAt" in data
 
-def test_delete_user(base_url, headers):
-    response = requests.delete(f"{base_url}/users/2", headers=headers)
-    assert response.status_code == 204  # Нет содержимого, но успешно
+# def test_delete_user(base_url, headers):
+#     response = requests.delete(f"{base_url}/users/2", headers=headers)
+#     assert response.status_code == 204  # Нет содержимого, но успешно
 
-@pytest.mark.xfail(reason="баг на стороне API")
-def test_create_user_without_body(base_url, headers):
-    response = requests.post(f"{base_url}/users", headers=headers)
-    assert response.status_code == 400 or response.status_code == 415  # зависит от API
+# @pytest.mark.xfail(reason="баг на стороне API")
+# def test_create_user_without_body(base_url, headers):
+#     response = requests.post(f"{base_url}/users", headers=headers)
+#     assert response.status_code == 400 or response.status_code == 415  # зависит от API
 
-def test_get_nonexistent_resource(base_url, headers):
-    response = requests.get(f"{base_url}/unknown/999", headers=headers)
-    assert response.status_code == 404
+# def test_get_nonexistent_resource(base_url, headers):
+#     response = requests.get(f"{base_url}/unknown/999", headers=headers)
+#     assert response.status_code == 404
 
-@pytest.mark.xfail(reason="reqres.in может возвращать 200 с некорректным API-ключом")
-def test_with_invalid_api_key(base_url):
-    headers = {"x-api-key": "invalid-key"}
-    response = requests.get(f"{base_url}/users/2", headers=headers)
-    assert response.status_code == 401
+# @pytest.mark.xfail(reason="reqres.in может возвращать 200 с некорректным API-ключом")
+# def test_with_invalid_api_key(base_url):
+#     headers = {"x-api-key": "invalid-key"}
+#     response = requests.get(f"{base_url}/users/2", headers=headers)
+#     assert response.status_code == 401
