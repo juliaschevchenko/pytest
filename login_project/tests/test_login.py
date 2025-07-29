0db0420b7fba9cbb1e1df25aae44df0c933a3c91
schevcha <j.shevcha@gmail.com>
2025-07-30 00:03:30 +0300

    broke ui-test

--- login_project/tests/test_login.py
+++ login_project/tests/test_login.py
@@ -9,7 +9,7 @@ from pages.login_page import LoginPage
 def test_successful_login(page):
     login_page = LoginPage(page)
     login_page.goto()
-    login_page.login('student', 'Password123')
+    login_page.login('student', 'Password12')
 
     # Проверка: заголовок и URL
     page.wait_for_url("**/logged-in-successfully/")
